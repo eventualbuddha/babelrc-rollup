cc4e876a8856e5ac3c04bf587bb44ad75a0a5858
Brian Donovan <me@brian-donovan.com>
2016-06-09 05:23:16 -0700

    Improve the build system.
    - Move bootstrap script into its own file, with comments.
    - Update rollup to include `__esModule` support.
    - Update other dependencies.
    - Make CJS & ES builds, skipping UMD.
    - Consolidate rollup config using `targets` option.
    - Use the probably-standard `.mjs` extension for ES modules.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
 import babel from 'rollup-plugin-babel';
-import babelrc from './dist/babelrc-rollup.m.js';
+import babelrc from './dist/babelrc-rollup.mjs';
 
 export default {
   entry: 'src/index.js',
   plugins: [babel(babelrc())],
   external: ['fs'],
-  globals: { fs: 'fs' }
+  targets: [
+    {
+      dest: 'dist/babelrc-rollup.mjs',
+      exports: 'named',
+      format: 'es6'
+    },
+    {
+      dest: 'dist/babelrc-rollup.js',
+      exports: 'named',
+      format: 'cjs'
+    }
+  ]
 };
--- rollup.es.config.js
+++
@@ -1,6 +0,0 @@
-import config from './rollup.config.js';
-
-config.dest = 'dist/babelrc-rollup.m.js';
-config.format = 'es6';
-
-export default config;
