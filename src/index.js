5cfa3be875cd7cdd2c8250e331136ea5f6e3a034
Brian Donovan <me@brian-donovan.com>
2016-08-08 16:37:02 -0700

    feat: update to work with babel v6.13.x by passing options (#4)
    BREAKING CHANGE: we no longer look for `-rollup` suffixed presets unless you specifically ask to. You can restore the old behavior as described in the README.
    
    Works around https://github.com/rollup/babel-preset-es2015-rollup/issues/7.

--- src/index.js
+++ src/index.js
@@ -7,49 +7,78 @@ function startsWith(string: string, prefix: string): boolean {
   return string.lastIndexOf(prefix, 0) === 0;
 }
 
+type Preset = any;
+
 type BabelConfig = {
-  filename?: string,
-  filenameRelative?: string,
-  presets?: Array<string>,
-  plugins?: Array<string>,
-  highlightCode?: boolean,
-  only?: ?(RegExp | string | Array<RegExp | string>),
-  ignore?: ?(RegExp | string | Array<RegExp | string>),
-  auxiliaryCommentBefore?: ?string,
-  auxiliaryCommentAfter?: ?string,
-  sourceMaps?: boolean,
-  inputSourceMap?: ?Object,
-  sourceMapTarget?: string,
-  sourceFileName?: string,
-  sourceRoot?: string,
-  moduleRoot?: string,
-  moduleIds?: boolean,
-  moduleId?: string,
-  getModuleId?: (moduleName: string) => ?string,
-  resolveModuleSource?: (source: string, filename: string) => ?string,
-  code?: boolean,
-  babelrc?: boolean,
-  ast?: boolean,
-  compact?: boolean | 'auto',
-  comments?: boolean,
-  shouldPrintComment?: (commentContents: string) => boolean,
-  env?: {[key: string]: BabelConfig},
-  retainLines?: boolean,
-  extends?: ?string,
+  filename?: string;
+  filenameRelative?: string;
+  presets?: Array<Preset>;
+  plugins?: Array<string>;
+  highlightCode?: boolean;
+  only?: ?(RegExp | string | Array<RegExp | string>);
+  ignore?: ?(RegExp | string | Array<RegExp | string>);
+  auxiliaryCommentBefore?: ?string;
+  auxiliaryCommentAfter?: ?string;
+  sourceMaps?: boolean;
+  inputSourceMap?: ?Object;
+  sourceMapTarget?: string;
+  sourceFileName?: string;
+  sourceRoot?: string;
+  moduleRoot?: string;
+  moduleIds?: boolean;
+  moduleId?: string;
+  getModuleId?: (moduleName: string) => ?string;
+  resolveModuleSource?: (source: string, filename: string) => ?string;
+  code?: boolean;
+  babelrc?: boolean;
+  ast?: boolean;
+  compact?: boolean | 'auto';
+  comments?: boolean;
+  shouldPrintComment?: (commentContents: string) => boolean;
+  env?: {[key: string]: BabelConfig};
+  retainLines?: boolean;
+  extends?: ?string;
 };
 
-export default function babelrc(path: string='.babelrc'): BabelConfig {
-  return configWithoutModules(JSON.parse(readFileSync(path, { encoding: 'utf8' })));
+type Options = {
+  path?: string;
+  config?: BabelConfig;
+  findRollupPresets?: boolean;
+  addModuleOptions?: boolean;
+  addExternalHelpersPlugin?: boolean;
+  resolve?: (path: string) => string;
+};
+
+const DEFAULT_OPTIONS = {
+  path: '.babelrc',
+  findRollupPresets: false,
+  addModuleOptions: true,
+  addExternalHelpersPlugin: true,
+  resolve
+};
+
+export default function babelrc(options: Options|string={}): BabelConfig {
+  if (typeof options === 'string') {
+    options = { path: options };
+  }
+
+  let resolvedOptions = { ...DEFAULT_OPTIONS, ...options };
+
+  if (!resolvedOptions.config && typeof resolvedOptions.path === 'string') {
+    resolvedOptions.config = JSON.parse(readFileSync(resolvedOptions.path, { encoding: 'utf8' }));
+  }
+
+  return configWithoutModules(resolvedOptions.config, resolvedOptions);
 }
 
-export function configWithoutModules(config: BabelConfig): BabelConfig {
+export function configWithoutModules(config: BabelConfig, options: Options): BabelConfig {
   let result = {};
 
   for (let key in config) {
     if (Object.prototype.hasOwnProperty.call(config, key)) {
       if (key === 'presets' && config.presets) {
         // Replace the es2015 preset with the es2015-rollup preset.
-        result.presets = config.presets.map(mapPreset);
+        result.presets = config.presets.map(preset => mapPreset(preset, options));
       } else if (key === 'plugins' && config.plugins) {
         // Remove any explicit module plugins, e.g. es2015-modules-commonjs.
         result.plugins = config.plugins.filter(
@@ -61,18 +90,57 @@ export function configWithoutModules(config: BabelConfig): BabelConfig {
     }
   }
 
+  if (options.addExternalHelpersPlugin) {
+    if (!result.plugins) {
+      result.plugins = ['external-helpers'];
+    } else {
+      result.plugins = [...result.plugins, 'external-helpers'];
+    }
+  }
+
   // Make sure babel does not look for the babelrc file.
   result.babelrc = false;
 
   return result;
 }
 
-function mapPreset(preset: string): string {
+function mapPreset(preset: any, options: Options): any {
+  let info = getPresetInfo(preset);
+
+  if (!info) {
+    return preset;
+  }
+
+  if (options.findRollupPresets && hasRollupVersionOfPreset(info.name, options.resolve || resolve)) {
+    return [`${info.name}-rollup`, info.options];
+  } else if (options.addModuleOptions) {
+    return [info.name, { ...info.options, modules: false }];
+  } else {
+    return preset;
+  }
+}
+
+function getPresetInfo(preset: any): ?{ name: string, options: Object } {
+  if (typeof preset === 'string') {
+    return { name: preset, options: {} };
+  } else if (Array.isArray(preset)) {
+    let name = preset[0];
+    let options = preset[1] || {};
+
+    if (typeof name === 'string' && typeof options === 'object') {
+      return { name, options };
+    }
+  }
+
+  return null;
+}
+
+function hasRollupVersionOfPreset(preset: string, resolve: (path: string) => string): boolean {
   try {
     // this will throw if it can't resolve it
     resolve(`babel-preset-${preset}-rollup`);
-    return `${preset}-rollup`;
+    return true;
   } catch (err) {
-    return preset;
+    return false;
   }
 }
